121968ec5519bb9ddba54357c20f9890a69f2cfa
Allisson Felipe <allissonfmbs95@gmail.com>
2024-12-27 13:44:57 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,7 +40,11 @@ public class Program {
 				}
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/Q/R/N): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid Value! Enter piece for promotion (B/Q/R/N): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
